da63e0c5b6ba0bc74e953e763fcc48260e18e2c3
Ajinkya Bhogade <ajinkya.bhogade@gmail.com>
2017-10-16 23:09:43 +0530

    second commit

--- src/main/java/com/infotech/app/TicketBookingManagmentApplication.java
+++ src/main/java/com/infotech/app/TicketBookingManagmentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketBookingManagmentApplication {
-	
+	//Ajinkya Bhogade
 	public static void main(String[] args) {
 		SpringApplication.run(TicketBookingManagmentApplication.class, args);
 	}
